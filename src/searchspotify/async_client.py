3333b1886b9baa6878731f960109b6cf76365832
TrueSaiyan <ufperth@protonmail.com>
2024-04-26 06:05:09 +0800

    Update async_client.py

--- src/searchspotify/async_client.py
+++ src/searchspotify/async_client.py
@@ -1,10 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2020-2023 (c) Randy W @xtdevs, @xtsea
+# Nimbus ~ UserBot
+# Copyright (C) 2023 NimbusTheCloud, ufoptg, @TrueSaiyan
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# This file is a part of < https://github.com/ufoptg/Nimbus/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/ufoptg/Nimbus/blob/main/LICENSE/>.
+# If not, see <https://www.gnu.org/licenses/>.
+
+# THIS FILE IS RESPONSABLE FOR DEALING WITH THE CLIENT
+
 import aiohttp
 import re
 
 import requests
 from bs4 import BeautifulSoup
 
-from . import calls
+from . import async_calls
 from .classes import Albums, Authenticator, Playlists, Results
 
 
